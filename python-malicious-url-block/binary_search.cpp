c880bb21c4fc7e0c74b979a0329ef93869f02bfe
walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2023-01-06 22:18:40 +0900

    txtから検索する機能を追加

---
+++ python-malicious-url-block/binary_search.cpp
@@ -0,0 +1,49 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+bool comment(const std::string& s){
+    if(s.at(0) == '#'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+bool search(const std::string& target, const std::string& path){
+    std::ifstream file(path);
+    std::string line;
+    std::vector<std::string> source;
+
+    while (std::getline(file, line)){
+        if(comment(line) == false){
+            source.push_back(line);
+        }
+
+    }
+
+    if(std::binary_search(source.begin(), source.end(), target)){
+        return true;
+    }else{
+        return false;
+    }
+
+
+
+}
+
+int main(){
+    std::string input, path;
+    std::cin >> input >> path;
+    if(search(input, path)){
+        std::cout << "Yes" << std::endl;
+    }else{
+        std::cout << "No" << std::endl;
+    }
+    return 0;
+}
+
